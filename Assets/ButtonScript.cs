c11dca339ce9cc8fcea5b76422effc757af5f99b
Srayan Jana <valorzard.kiko@outlook.com>
2022-10-23 10:09:15 -0700

    line for the door trigger

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 20;
+
     void Start()
     {
-        
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.2f;
+
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+
+        // make line between two points
+        lineRenderer.positionCount = 2;
+
+        var points = new Vector3[2];
+        points[0] = transform.position;
+        points[1] = transform.GetChild(0).position;
+        lineRenderer.SetPositions(points);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
